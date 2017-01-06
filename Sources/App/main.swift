fb0c60cd36467b2301b3a0a283572f193778482e
Imhoisi Otokhagua <imhoisi@icloud.com>
2017-01-06 23:25:36 +0300

    .

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,6 +7,10 @@ try drop.addProvider(VaporMongo.Provider.self)
 
 let rc = RestaurantController()
 
+drop.get("/") { request in
+    return "Hello World!!"
+}
+
 drop.middleware.append(VersionMiddleware())
 
 drop.preparations.append(Restaurant.self)
