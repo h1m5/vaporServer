928392bb80d164caa9a808ece4e288117b76732a
Imhoisi Otokhagua <imhoisi@icloud.com>
2017-01-09 20:14:12 +0300

    refactored

--- Sources/App/Controllers/RestaurantController.swift
+++ Sources/App/Controllers/RestaurantController.swift
@@ -14,28 +14,20 @@ final class RestaurantController{
     
     init() {
         drop.resource("api/restaurants", self)
-        drop.get("api/images", Restaurant.self, handler: serveGridFS)
+        drop.get("api/images", String.self, handler: serveGridFS)
     }
     
     let gridFS: GridFS = {
         let server = try! MongoKitten.Server(mongo_remoteUri)
+//        let server = try! MongoKitten.Server(at: mongo_localHost)
         let db = server["mofire-db"]
         let gfs = try! GridFS(in: db)
         return gfs
     }()
     
-    func serveGridFS(_ req: Request, restaurant: Restaurant) throws -> ResponseRepresentable {
-        return try gridFS.serve(byId: ObjectId.init(restaurant.imageId!))
+    func serveGridFS(_ req: Request, imageId: String) throws -> ResponseRepresentable {
+        return try gridFS.serve(byId: ObjectId.init(imageId))
     }
-    
-//    func saveImage(_ req: Request) throws -> ResponseRepresentable {
-//        if let contentType = req.headers["Content-Type"], contentType.contains("image/png"), let bytes = req.body.bytes {
-//            guard let image = NSImage(data: Data(bytes)) else { return "not awesome"}
-//            print(image)
-//            return JSON(["Testawesome":"awesome123"])
-//        }
-//        return JSON(["test":"123"])
-//    }
     
     func getAll(_ req: Request) throws -> ResponseRepresentable {
         return try Restaurant.all().makeNode().converted(to: JSON.self)
@@ -49,8 +41,13 @@ final class RestaurantController{
         var restaurant = try req.postRestaurant()
         guard let imageMultipart = req.multipart?[restaurant.imageName!] else { throw Abort.badRequest }
         restaurant.imageId = try gridFS.store(file: imageMultipart.file!).hexString
+        restaurant.imageUri = getUrlForImage(req: req, r: restaurant)
         try restaurant.save()
         return restaurant
+    }
+    
+    private func getUrlForImage(req : Request, r : Restaurant) -> String {
+        return  "\(req.uri.deletingLastPathComponent().description)/images/\(r.imageId!)"
     }
     
     func delete(_ req: Request, restaurant: Restaurant) throws -> ResponseRepresentable {
@@ -64,7 +61,14 @@ final class RestaurantController{
         
         guard var new = try Restaurant.init(from: restaurant) else { throw Abort.badRequest }
         
-        //parameter update using json
+        updateParameters(using: json, on: &new)
+        
+        try new.save()
+        
+        return new
+    }
+    
+    private func updateParameters(using json:JSON, on new: inout Restaurant) {
         new.name = json["name"] != nil ? json["name"]?.string : new.name
         new.type = json["type"] != nil ? json["type"]?.string : new.type
         new.location = json["location"] != nil ? json["location"]?.string : new.location
@@ -75,10 +79,6 @@ final class RestaurantController{
         new.visited = json["visited"] != nil ? (json["visited"]?.bool)! : new.visited
         new.rating = json["rating"] != nil ? (json["rating"]?.string)! : new.rating
         new.exists = json["exists"] != nil ? (json["exists"]?.bool)! : new.exists
-        
-        try new.save()
-        
-        return new
     }
     
     func clear(_ req: Request) throws -> ResponseRepresentable {
